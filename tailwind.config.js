3eb7c6642dea30841b6b1dc5346e61bc1c098443
Debakor10 <debakor10@gmail.com>
2022-10-13 14:04:12 +0600

    footer update

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,9 +20,9 @@ module.exports = {
       center: true,
       padding: "1rem",
       screens: {
-        lg: "1200px",
-        xl: "1200px",
-        "2xl": "1200px",
+        lg: "1280px",
+        xl: "1280px",
+        "2xl": "1280px",
       },
     },
   },
